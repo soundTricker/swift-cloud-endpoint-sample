d6a5ae0e399e1403ef91d950fb7dca24a28e062f
keisuke oohashi <keisuke.oohashi@gmail.com>
2014-06-09 18:02:29 +0900

    Initial Commit

---
+++ io.github.soundtricker.swift-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  io.github.soundtricker.swift-sample
+//
+//  Created by 富永啓介 on H26/06/09.
+//  Copyright (c) 平成26年 大橋啓介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ io.github.soundtricker.swift-sampleTests/io_github_soundtricker_swift_sampleTests.swift
@@ -0,0 +1,35 @@
+//
+//  io_github_soundtricker_swift_sampleTests.swift
+//  io.github.soundtricker.swift-sampleTests
+//
+//  Created by 富永啓介 on H26/06/09.
+//  Copyright (c) 平成26年 大橋啓介. All rights reserved.
+//
+
+import XCTest
+
+class io_github_soundtricker_swift_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
